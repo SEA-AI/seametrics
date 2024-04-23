e61a41d1c5c0441dbd4b27909d3c8705330797bb
franziska-denk <denk.m.franziska@gmail.com>
2024-04-23 10:35:13 +0200

    refactor to match signature of similar det function

--- seametrics/segmentation/utils.py
+++ seametrics/segmentation/utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import fiftyone as fo
+
 def payload_to_seg_metric(payload: dict, model_name: str, label2id: dict=None):
     """
     Convert data in the standard payload format to the format expected by the segmentation metric.
@@ -16,22 +18,34 @@ def payload_to_seg_metric(payload: dict, model_name: str, label2id: dict=None):
 
     if label2id is None:
         label2id = dict()
-        
-    frames = []
+    
+    pred_frames = []
+    gt_frames = []
     for seq in payload["sequence_list"]:
         h, w = payload["sequences"][seq]["resolution"]
         preds = payload["sequences"][seq][model_name] # n_frames, m_detections
+        gts = payload["sequences"][seq][payload["gt_field_name"]] # n_frames, m_detections
         for frame_dets in preds:
-            gt_mask = np.zeros((h, w, 2))
-            for instance_idx, det in enumerate(sorted(frame_dets, 
-                                                      key=lambda det: det["mask"].sum(), 
-                                                      reverse=True)): # put large masks below smaller masks
-                start_x, start_y = int(det["bounding_box"][0]*w), int(det["bounding_box"][1]*h)
-                y, x = np.where(det["mask"] == 1)
-                y += int(start_y)
-                x += int(start_x)
-                if det["label"] not in label2id:
-                    label2id[det["label"]] = len(label2id)
-                gt_mask[y,x] = np.array([label2id[det["label"]], instance_idx])
-            frames.append(gt_mask)
-    return np.stack(frames, axis=0), label2id
+            pred_frames.append(multiple_masks_to_single_mask(frame_dets, h, w, label2id))
+        for frame_dets in gts:
+            gt_frames.append(multiple_masks_to_single_mask(frame_dets, h, w, label2id))
+
+    return np.stack(pred_frames, axis=0), np.stack(gt_frames, axis=0), label2id
+
+
+def multiple_masks_to_single_mask(frame_dets: list[fo.Detection],
+                                  h: int,
+                                  w: int,
+                                  label2id: dict):
+    single_mask = np.zeros((h, w, 2))
+    for instance_idx, det in enumerate(sorted(frame_dets, 
+                                            key=lambda det: det["mask"].sum(), 
+                                            reverse=True)): # put large masks below smaller masks
+        start_x, start_y = int(det["bounding_box"][0]*w), int(det["bounding_box"][1]*h)
+        y, x = np.where(det["mask"] == 1)
+        y += int(start_y)
+        x += int(start_x)
+        if det["label"] not in label2id:
+            label2id[det["label"]] = len(label2id)
+        single_mask[y,x] = np.array([label2id[det["label"]], instance_idx])
+    return single_mask
