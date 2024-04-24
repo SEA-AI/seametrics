b1e5f20cd3735dca846cb2d5f4ac9b815ba71c98
franziska-denk <denk.m.franziska@gmail.com>
2024-04-24 11:48:03 +0200

    add num_samples as input parameter

--- seametrics/payload/processor.py
+++ seametrics/payload/processor.py
@@ -25,6 +25,7 @@ class PayloadProcessor:
         sequence_list: List[str] = None,
         data_type: Literal["rgb", "thermal"] = "thermal",
         excluded_classes: List[str] = None,
+        num_samples: int = None
     ):
         """
         Initializes a PayloadProcessor object.
@@ -52,7 +53,7 @@ class PayloadProcessor:
         self.validate_input_parameters(dataset_name)
         self.dataset: fo.Dataset = None
         self.payload: Payload = None
-        self.num_samples: int = None,
+        self.num_samples = num_samples,
         self.compute_payload()
         logger.info(f"Initialized PayloadProcessor for dataset: {dataset_name}")
 
