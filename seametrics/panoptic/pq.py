6abe59d4d95645e6e20306a5820e3c4ae2b6b50b
franziska-denk <denk.m.franziska@gmail.com>
2024-04-24 12:56:33 +0200

    move data to gpu if available

--- seametrics/panoptic/pq.py
+++ seametrics/panoptic/pq.py
@@ -21,6 +21,22 @@ class PanopticQuality():
         self.things = things
         self.stuffs = stuffs
         self.metric = PQ(things=things, stuffs=stuffs, allow_unknown_preds_category=True)
+        self.device = self.select_device()
+
+
+    @staticmethod
+    def select_device():
+        # Check for CUDA GPU availability
+        if torch.cuda.is_available():
+            return torch.device('cuda')
+        
+        # Check for MPS availability (for macOS on Apple Silicon)
+        elif torch.backends.mps.is_available():
+            return torch.device('mps')
+        
+        # Default to CPU if neither CUDA nor MPS is available
+        else:
+            return torch.device('cpu')
 
     def update(self,
                preds: torch.Tensor,
@@ -41,7 +57,9 @@ class PanopticQuality():
         if type(preds) == np.ndarray:
             preds = torch.from_numpy(preds)
         if type(targets) == np.ndarray:
-            targets = torch.from_numpy(targets)    
+            targets = torch.from_numpy(targets)
+
+        preds, targets = preds.to(self.device), targets.to(self.device)    
 
         self.metric.update(preds, targets)
         print("Added data ...")
