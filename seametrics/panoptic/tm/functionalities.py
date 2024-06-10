edf0293ba9e39b6cc7bdb57dcfb2b3ad32b8c504
franziska-denk <denk.m.franziska@gmail.com>
2024-06-10 16:50:52 +0200

    last bugfix for today

--- seametrics/panoptic/tm/functionalities.py
+++ seametrics/panoptic/tm/functionalities.py
@@ -5,9 +5,38 @@ from torch import Tensor
 from torchmetrics.functional.detection._panoptic_quality_common import (
     _calculate_iou,
     _get_color_areas,
-    _Color,
-    _filter_false_negatives
+    _Color
 )
+
+def _filter_false_negatives(
+    target_areas: Dict[_Color, Tensor],
+    target_segment_matched: Set[_Color],
+    intersection_areas: Dict[Tuple[_Color, _Color], Tensor],
+    void_color: Tuple[int, int],
+    area: Tuple[float]
+) -> Iterator[int]:
+    """Filter false negative segments and yield their category IDs.
+
+    False negatives occur when a ground truth segment is not matched with a prediction.
+    Areas that are mostly void in the prediction are ignored.
+
+    Args:
+        target_areas: Mapping from colors of the ground truth segments to their extents.
+        target_segment_matched: Set of ground truth segments that have been matched to a prediction.
+        intersection_areas: Mapping from tuples of `(pred_color, target_color)` to their extent.
+        void_color: An additional color that is masked out during metrics calculation.
+
+    Yields:
+        Category IDs of segments that account for false negatives.
+
+    """
+    lower, upper = area
+    false_negative_colors = set(target_areas) - target_segment_matched
+    false_negative_colors.discard(void_color)
+    for target_color in false_negative_colors:
+        void_target_area = intersection_areas.get((void_color, target_color), 0)
+        if void_target_area / target_areas[target_color] <= 0.5 and (target_areas[target_color] >= lower) and (target_areas[target_color] < upper):
+            yield target_color[0]
 
 def _filter_false_positives(
     pred_areas: Dict[_Color, Tensor],
