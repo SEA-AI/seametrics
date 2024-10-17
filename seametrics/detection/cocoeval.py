81905d3a097490b4fb15b94ceac2f979fb6bd59c
franziska-denk <denk.m.franziska@gmail.com>
2024-10-17 14:31:49 +0200

    test

--- seametrics/detection/cocoeval.py
+++ seametrics/detection/cocoeval.py
@@ -597,9 +597,12 @@ class COCOeval:
 
             # handle case where tp, fp, fn and dup are empty (no gt and no dt)
             if all([not np.any(m) for m in [tp, fp, fn, dup, fpi]]):
-                tp, fp, fn, dup, fpi =[0] * 5
+                tp, fp, fn, dup, fpi = [-1] * 5
             else:
                 tp, fp, fn, dup, fpi = [e.item() for e in [tp, fp, fn, dup, fpi]]
+
+            if tp == -1 and fp == -1 and fn == -1:
+                tp, fp, fn, dup, fpi = [0] * 5
             
             # compute precision, recall, f1
             pr = -1 if tp + fp == 0 else tp / (tp + fp)
