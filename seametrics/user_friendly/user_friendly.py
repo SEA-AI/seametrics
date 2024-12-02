eaca769291b027dbda4f0258b40629d2f7d129a4
hichem-sea <hichem.abdellali@sea.ai>
2024-12-02 09:56:48 +0100

    CV-184 migrate the basic functions

---
+++ seametrics/user_friendly/user_friendly.py
@@ -0,0 +1,59 @@
+from seametrics.user_friendly.utils import *
+
+
+class UserFriendlyMetrics:
+    """
+    Class for computing UserFriendly metrics.
+
+    Parameters
+    ----------
+
+    Attributes
+    ----------
+
+    Methods
+    -------
+    update(predictions, ground_truth_det)
+        Update the predictions and ground truth detections.
+    compute()
+        Compute the UserFriendly metrics across the sequence.
+
+    """
+
+    def __init__(
+        self, payload, max_iou, filters, recognition_thresholds, debug
+    ) -> None:
+        """
+        Initialize the UserFriendly class.
+
+        Parameters
+        ----------
+
+        """
+        self.payload = payload
+        self.max_iou = max_iou
+        self.filters = filters
+        self.recognition_thresholds = recognition_thresholds
+        self.debug = debug
+
+    def update(self, predictions, ground_truth_det) -> None:
+        """
+        Update the predictions and ground truth detections.
+
+        Parameters
+        ----------.
+
+        """
+
+        print("user-friendly-metrics updated")
+
+    def _compute(self):
+        """Returns the scores"""
+        # TODO: Compute the different scores of the module
+        return calculate_from_payload(
+            self.payload,
+            self.max_iou,
+            self.filters,
+            self.recognition_thresholds,
+            self.debug,
+        )
