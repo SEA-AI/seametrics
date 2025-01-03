10098cc1cb4d077f01b2eb8597097c87b505b61a
Kevin Serrano <35779409+kevinconka@users.noreply.github.com>
2025-01-03 10:17:21 +0100

    PrecisionRecallF1 fix concatenating empty lists (#37)
    * gracefully handle no images (empty lists) as input
    
    * add unit test for case where nImgs == 0 in det-metrics
    
    * remove torchmetrics PrRecF1 tests since torchmetrics as backend is explicitly disabled

--- tests/detection/test_pr_rec_f1.py
+++ tests/detection/test_pr_rec_f1.py
@@ -1,7 +1,29 @@
 import numpy as np
 from seametrics.detection import PrecisionRecallF1Support
 
-def test_empty_input_with_default_args():
+def test_no_images_with_default_args():
+    metric = PrecisionRecallF1Support()
+    preds = []
+    target = []
+    metric.update(preds, target)
+    results = metric.compute()
+    for key, val in results["metrics"].items():
+        assert key == "all"
+        assert val["iouThr"] == "0.50"
+        assert val["maxDets"] == 100
+        assert val["tp"] == 0
+        assert val["fp"] == 0
+        assert val["fn"] == 0
+        assert val["duplicates"] == 0
+        assert val["precision"] == -1
+        assert val["recall"] == -1
+        assert val["f1"] == -1
+        assert val["support"] == 0
+        assert val["fpi"] == 0
+        assert val["nImgs"] == 0
+
+
+def test_no_preds_and_target_with_default_args():
     metric = PrecisionRecallF1Support()
     preds = [
         dict(
@@ -18,20 +40,21 @@ def test_empty_input_with_default_args():
     ]
     metric.update(preds, target)
     results = metric.compute()
-    for key, val in results['metrics'].items():
-        assert key == 'all'
-        assert val['iouThr'] == '0.50'
-        assert val['maxDets'] == 100
-        assert val['tp'] == 0
-        assert val['fp'] == 0
-        assert val['fn'] == 0
-        assert val['duplicates'] == 0
-        assert val['precision'] == -1
-        assert val['recall'] == -1
-        assert val['f1'] == -1
-        assert val['support'] == 0
-        assert val['fpi'] == 0
-        assert val['nImgs'] == 1
+    for key, val in results["metrics"].items():
+        assert key == "all"
+        assert val["iouThr"] == "0.50"
+        assert val["maxDets"] == 100
+        assert val["tp"] == 0
+        assert val["fp"] == 0
+        assert val["fn"] == 0
+        assert val["duplicates"] == 0
+        assert val["precision"] == -1
+        assert val["recall"] == -1
+        assert val["f1"] == -1
+        assert val["support"] == 0
+        assert val["fpi"] == 0
+        assert val["nImgs"] == 1
+
 
 def test_empty_gt_false_pred_with_default_args():
     metric = PrecisionRecallF1Support()
@@ -50,20 +73,21 @@ def test_empty_gt_false_pred_with_default_args():
     ]
     metric.update(preds, target)
     results = metric.compute()
-    for key, val in results['metrics'].items():
-        assert key == 'all'
-        assert val['iouThr'] == '0.50'
-        assert val['maxDets'] == 100
-        assert val['tp'] == 0
-        assert val['fp'] == 1
-        assert val['fn'] == 0
-        assert val['duplicates'] == 0
-        assert val['precision'] == 0
-        assert val['recall'] == -1
-        assert val['f1'] == -1
-        assert val['support'] == 0
-        assert val['fpi'] == 1
-        assert val['nImgs'] == 1
+    for key, val in results["metrics"].items():
+        assert key == "all"
+        assert val["iouThr"] == "0.50"
+        assert val["maxDets"] == 100
+        assert val["tp"] == 0
+        assert val["fp"] == 1
+        assert val["fn"] == 0
+        assert val["duplicates"] == 0
+        assert val["precision"] == 0
+        assert val["recall"] == -1
+        assert val["f1"] == -1
+        assert val["support"] == 0
+        assert val["fpi"] == 1
+        assert val["nImgs"] == 1
+
 
 def test_empty_pred_missed_gt_with_default_args():
     metric = PrecisionRecallF1Support()
@@ -82,18 +106,17 @@ def test_empty_pred_missed_gt_with_default_args():
     ]
     metric.update(preds, target)
     results = metric.compute()
-    for key, val in results['metrics'].items():
-        assert key == 'all'
-        assert val['iouThr'] == '0.50'
-        assert val['maxDets'] == 100
-        assert val['tp'] == 0
-        assert val['fp'] == 0
-        assert val['fn'] == 1
-        assert val['duplicates'] == 0
-        assert val['precision'] == -1
-        assert val['recall'] == 0
-        assert val['f1'] == -1
-        assert val['support'] == 1
-        assert val['fpi'] == 0
-        assert val['nImgs'] == 1
-        
+    for key, val in results["metrics"].items():
+        assert key == "all"
+        assert val["iouThr"] == "0.50"
+        assert val["maxDets"] == 100
+        assert val["tp"] == 0
+        assert val["fp"] == 0
+        assert val["fn"] == 1
+        assert val["duplicates"] == 0
+        assert val["precision"] == -1
+        assert val["recall"] == 0
+        assert val["f1"] == -1
+        assert val["support"] == 1
+        assert val["fpi"] == 0
+        assert val["nImgs"] == 1
