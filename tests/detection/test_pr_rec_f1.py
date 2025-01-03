6e412d6ef0483fd1083ece044f824be4a8158128
Kevin Serrano <kserranov@gmail.com>
2025-01-03 10:03:50 +0100

    add unit test for case where nImgs == 0 in det-metrics

--- tests/detection/test_pr_rec_f1.py
+++ tests/detection/test_pr_rec_f1.py
@@ -1,8 +1,45 @@
+from typing import Type
+import pytest
 import numpy as np
-from seametrics.detection import PrecisionRecallF1Support
+from seametrics.detection.np.pr_rec_f1 import PrecisionRecallF1Support as PRF1Np
 
-def test_empty_input_with_default_args():
-    metric = PrecisionRecallF1Support()
+backends = [PRF1Np]
+
+# Try importing the torchmetrics-based backend
+try:
+    from seametrics.detection.tm.pr_rec_f1 import PrecisionRecallF1Support as PRF1Tm
+
+    backends.append(PRF1Tm)
+except ImportError:
+    print("TorchMetrics not installed, skipping corresponding tests.")
+
+
+@pytest.mark.parametrize("MetricClass", backends)
+def test_no_images_with_default_args(MetricClass: Type[PRF1Np]):
+    metric = MetricClass()
+    preds = []
+    target = []
+    metric.update(preds, target)
+    results = metric.compute()
+    for key, val in results["metrics"].items():
+        assert key == "all"
+        assert val["iouThr"] == "0.50"
+        assert val["maxDets"] == 100
+        assert val["tp"] == 0
+        assert val["fp"] == 0
+        assert val["fn"] == 0
+        assert val["duplicates"] == 0
+        assert val["precision"] == -1
+        assert val["recall"] == -1
+        assert val["f1"] == -1
+        assert val["support"] == 0
+        assert val["fpi"] == 0
+        assert val["nImgs"] == 0
+
+
+@pytest.mark.parametrize("MetricClass", backends)
+def test_no_preds_and_target_with_default_args(MetricClass: Type[PRF1Np]):
+    metric = MetricClass()
     preds = [
         dict(
             boxes=np.array([]),
@@ -18,23 +55,25 @@ def test_empty_input_with_default_args():
     ]
     metric.update(preds, target)
     results = metric.compute()
-    for key, val in results['metrics'].items():
-        assert key == 'all'
-        assert val['iouThr'] == '0.50'
-        assert val['maxDets'] == 100
-        assert val['tp'] == 0
-        assert val['fp'] == 0
-        assert val['fn'] == 0
-        assert val['duplicates'] == 0
-        assert val['precision'] == -1
-        assert val['recall'] == -1
-        assert val['f1'] == -1
-        assert val['support'] == 0
-        assert val['fpi'] == 0
-        assert val['nImgs'] == 1
+    for key, val in results["metrics"].items():
+        assert key == "all"
+        assert val["iouThr"] == "0.50"
+        assert val["maxDets"] == 100
+        assert val["tp"] == 0
+        assert val["fp"] == 0
+        assert val["fn"] == 0
+        assert val["duplicates"] == 0
+        assert val["precision"] == -1
+        assert val["recall"] == -1
+        assert val["f1"] == -1
+        assert val["support"] == 0
+        assert val["fpi"] == 0
+        assert val["nImgs"] == 1
 
-def test_empty_gt_false_pred_with_default_args():
-    metric = PrecisionRecallF1Support()
+
+@pytest.mark.parametrize("MetricClass", backends)
+def test_empty_gt_false_pred_with_default_args(MetricClass: Type[PRF1Np]):
+    metric = MetricClass()
     preds = [
         dict(
             boxes=np.array([[258.0, 41.0, 606.0, 285.0]]),
@@ -50,23 +89,25 @@ def test_empty_gt_false_pred_with_default_args():
     ]
     metric.update(preds, target)
     results = metric.compute()
-    for key, val in results['metrics'].items():
-        assert key == 'all'
-        assert val['iouThr'] == '0.50'
-        assert val['maxDets'] == 100
-        assert val['tp'] == 0
-        assert val['fp'] == 1
-        assert val['fn'] == 0
-        assert val['duplicates'] == 0
-        assert val['precision'] == 0
-        assert val['recall'] == -1
-        assert val['f1'] == -1
-        assert val['support'] == 0
-        assert val['fpi'] == 1
-        assert val['nImgs'] == 1
+    for key, val in results["metrics"].items():
+        assert key == "all"
+        assert val["iouThr"] == "0.50"
+        assert val["maxDets"] == 100
+        assert val["tp"] == 0
+        assert val["fp"] == 1
+        assert val["fn"] == 0
+        assert val["duplicates"] == 0
+        assert val["precision"] == 0
+        assert val["recall"] == -1
+        assert val["f1"] == -1
+        assert val["support"] == 0
+        assert val["fpi"] == 1
+        assert val["nImgs"] == 1
 
-def test_empty_pred_missed_gt_with_default_args():
-    metric = PrecisionRecallF1Support()
+
+@pytest.mark.parametrize("MetricClass", backends)
+def test_empty_pred_missed_gt_with_default_args(MetricClass: Type[PRF1Np]):
+    metric = MetricClass()
     preds = [
         dict(
             boxes=np.array([]),
@@ -82,18 +123,17 @@ def test_empty_pred_missed_gt_with_default_args():
     ]
     metric.update(preds, target)
     results = metric.compute()
-    for key, val in results['metrics'].items():
-        assert key == 'all'
-        assert val['iouThr'] == '0.50'
-        assert val['maxDets'] == 100
-        assert val['tp'] == 0
-        assert val['fp'] == 0
-        assert val['fn'] == 1
-        assert val['duplicates'] == 0
-        assert val['precision'] == -1
-        assert val['recall'] == 0
-        assert val['f1'] == -1
-        assert val['support'] == 1
-        assert val['fpi'] == 0
-        assert val['nImgs'] == 1
-        
+    for key, val in results["metrics"].items():
+        assert key == "all"
+        assert val["iouThr"] == "0.50"
+        assert val["maxDets"] == 100
+        assert val["tp"] == 0
+        assert val["fp"] == 0
+        assert val["fn"] == 1
+        assert val["duplicates"] == 0
+        assert val["precision"] == -1
+        assert val["recall"] == 0
+        assert val["f1"] == -1
+        assert val["support"] == 1
+        assert val["fpi"] == 0
+        assert val["nImgs"] == 1
